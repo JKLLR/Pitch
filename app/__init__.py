4e3a75f694250143c435c6db5069c97943bc5b63
JKLLR <jeffhuria@gmail.com>
2022-05-08 17:14:13 +0300

    Created application factory function and instanciated bootstrap

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap= Bootstrap()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    #creating application configurations
+    app.config.from_object(config_options[config_name])
+
+    #initializing extensions
+    bootstrap.init_app(app)
+
+
+    return app
